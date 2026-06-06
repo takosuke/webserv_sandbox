4bcbf75aefeb27674203e088f0af1999e06958c5
takosuke <mariocampos0001@gmail.com>
2026-06-06 17:25:14 +0200

    fixed ServerConnection::handle where only one client connection was being created per EpollLoop tick even if there was multiple incoming clients

--- src/ServerConnection.cpp
+++ src/ServerConnection.cpp
@@ -7,6 +7,7 @@
 #include "ServerConnection.hpp"
 #include "ClientConnection.hpp"
 #include "EpollLoop.hpp"
+#include "Logger.hpp"
 #include "utils.hpp"
 
 void	ServerConnection::handle(uint32_t events) {
@@ -15,12 +16,18 @@ void	ServerConnection::handle(uint32_t events) {
 		EpollLoop::get_instance().del(this);
 		return ;
 	}
-	sockaddr_in client_addr;
-	socklen_t	client_len = sizeof(client_addr);
-	int client_fd = accept(fd, (sockaddr*)&client_addr, &client_len);
-	if (client_fd < 0) return; // TODO handle error?
-	set_nonblocking(client_fd);
-	ClientConnection *client_conn = new ClientConnection(client_fd, http, addr);
-
-	EpollLoop::get_instance().add(client_conn);
+	while (true) {
+		sockaddr_in client_addr;
+		socklen_t	client_len = sizeof(client_addr);
+		int client_fd = accept(fd, (sockaddr*)&client_addr, &client_len);
+		if (client_fd < 0) {
+			if (errno == EAGAIN || errno == EWOULDBLOCK)
+				break;
+			LOG_ERROR("ERROR") << "client_fd: " << client_fd << " connection error" << std::endl;
+			break;
+		}
+		set_nonblocking(client_fd);
+		ClientConnection *client_conn = new ClientConnection(client_fd, http, addr);
+		EpollLoop::get_instance().add(client_conn);
+	}
 }
