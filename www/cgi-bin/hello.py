3dae1f8133c877f12a96e3950d4d4b1ca5eaea9c
takosuke <mariocampos0001@gmail.com>
2026-06-09 12:46:02 +0200

    basic post testing in test cgi script

--- www/cgi-bin/hello.py
+++ www/cgi-bin/hello.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-body = "Hello, World!\n"
+body = "Hello, World!" + sys.stdin.read() + "!!!!\n"
 
 sys.stdout.write("Content-Type: text/plain\r\n")
 sys.stdout.write("Content-Length: " + str(len(body)) + "\r\n")
